debedfb8c65d85b5f18812638fea8cc8d8a52e54
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-06-29 21:08:23 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,8 +1,8 @@
 start,end=[int(x) for x in input().split()]
-for val in range(start+1,end):
+for val in range(start,end+1):
    if val>1:
       for n in range(2,val):
-         if (val%n)==0:
+         if(val%n)==0:
             break
-         else:
+          else:
             print(val,end=" ")
